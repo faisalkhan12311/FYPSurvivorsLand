b1be51d1584b06506684e6ccc3fb9328038f5c95
faisalkhan12311 <faisalkhan96123@gmail.com>
2020-06-01 13:02:05 +0500

    update1

---
+++ Survivors Land 2/Assets/Zombie.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class Zombie : MonoBehaviour
+{
+    public NavMeshAgent nav;
+    public Transform player;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        nav.SetDestination(player.position);
+    }
+}
